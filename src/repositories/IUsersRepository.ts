e299689a891aa2bc5dda8c1dde093f1539e4d8aa
mb4ndeira <mbandeira.cs@gmail.com>
2022-05-26 14:52:08 +0000

    feat: add the possibility of null return to users repository interface find by e-mail method

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -1,7 +1,7 @@
 import { User } from '../entities/User' 
 
 interface IUsersRepository {
-    findByEmail(email: string): Promise<User>;
+    findByEmail(email: string): Promise<User | null>;
     save(user: User): Promise<void>;
 }
 
